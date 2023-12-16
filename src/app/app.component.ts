d5e53ac9f7338a84db87b2329f40f09afd0176ba
Rutuja_Khandpekar <khandpekarrutuja@gmail.com>
2023-12-17 00:38:43 +0100

    Added google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { GoogleMapsModule } from '@angular/google-maps';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, GoogleMapsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'highway-map-autobahn';
+  highways: any;
+  roadWorks: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    this.http
+      .get<any>('https://verkehr.autobahn.de/o/autobahn/')
+      .subscribe((data) => {
+        this.highways = data;
+      });
+    console.log(this.highways);
+    const road = this.highways.roads[0];
+    this.http
+      .get<any>(
+        'https://verkehr.autobahn.de/o/autobahn/' + road + '/services/roadworks'
+      )
+      .subscribe((data) => {
+        this.roadWorks = data;
+        console.log(this.roadWorks);
+      });
+  }
+
+  display: any;
+  center: google.maps.LatLngLiteral = {
+    lat: 51.1657,
+    lng: 10.4515,
+  };
+  zoom = 6;
+  title = 'Highway Map';
+
+  /*------------------------------------------
+    --------------------------------------------
+    moveMap()
+    --------------------------------------------
+    --------------------------------------------*/
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.center = event.latLng.toJSON();
+  }
+
+  /*------------------------------------------
+    --------------------------------------------
+    move()
+    --------------------------------------------
+    --------------------------------------------*/
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNoopAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideNoopAnimations(),
+    importProvidersFrom(HttpClientModule),
+  ],
 };
